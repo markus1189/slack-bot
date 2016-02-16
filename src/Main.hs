666ce388d24541fdf3f619d5642c885f1c3ffd15
Markus Hauck <markus1189@gmail.com>
2016-02-16 20:51:57 +0100

    Standard reply when spoken to

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,14 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
 import           Control.Concurrent (forkIO)
 import           Control.Lens (view,strict)
 import           Control.Lens.Operators hiding ((.=))
+import           Control.Monad.State (runStateT)
+import           Control.Monad.State.Class (MonadState, put, get)
 import           Data.Aeson (Value(..), object, (.=), eitherDecode)
 import           Data.Aeson.Encode (encode)
 import           Data.Aeson.Lens
@@ -14,15 +16,18 @@ import           Data.ByteString.Lazy (ByteString)
 import           Data.Coerce (coerce)
 import           Data.Either (isRight)
 import           Data.Either.Combinators (fromRight')
+import           Data.Foldable (traverse_)
 import           Data.List.Split (splitOn)
 import           Data.Map (Map)
 import qualified Data.Map as Map
+import           Data.Maybe (fromMaybe)
 import           Data.Monoid ((<>))
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import           Network.WebSockets (sendClose, receiveData, sendTextData, Connection, WebSocketsData)
-import           Network.Wreq (get,responseBody)
+import           Network.Wreq (responseBody)
+import qualified Network.Wreq as Wreq
 import           Pipes
 import qualified Pipes.Concurrent as P
 import qualified Pipes.Prelude as P
@@ -34,6 +39,7 @@ import           Network.Slack.Types
 data State = State {stateReconnectUrl :: Maybe ReconnectUrl
                    ,stateUserPresence :: Map Text Presence
                    ,stateIdToName :: Map Text Text
+                   ,stateNextId :: Integer
                    }
 
 wsProd :: (WebSocketsData a, MonadIO m) => Connection -> Producer a m ()
@@ -56,7 +62,7 @@ wsProdCons buf c = do
 main :: IO ()
 main = do
   [token] <- getArgs
-  resp <- get ("https://slack.com/api/rtm.start?token=" <> token)
+  resp <- Wreq.get ("https://slack.com/api/rtm.start?token=" <> token)
   let body = resp ^. responseBody
       uri = body ^?! key "url" . _String
       users :: [User]
@@ -71,32 +77,37 @@ main = do
               (Map.union
                  (Map.fromList (map (\u -> (coerce (userId u), userName u)) users))
                  (Map.fromList (map (\c -> (coerce (chanId c), chanName c)) channels)))
-      translateId :: Text -> Text
-      translateId i = Map.findWithDefault i i (stateIdToName state)
+              1
   runSecureClient host 443 path $ \c -> do
     (output,input) <- wsProdCons (P.newest 50) c
-    runEffect $ P.fromInput input
-            >-> P.tee (P.map (T.unpack . T.decodeUtf8 . view strict)
-            >-> P.stdoutLn)
+    flip runStateT state $ runEffect $ P.fromInput input
+            >-> P.tee (P.map (T.unpack . T.decodeUtf8 . view strict) >-> P.stdoutLn)
             >-> P.map parse
-            >-> P.filter isRight
-            >-> P.map fromRight'
-            >-> P.filter (\e -> case e of
-                                  PresenceChangeEvt _ _ -> True
-                                  _ -> False)
-            >-> P.map (\(PresenceChangeEvt usr p) ->
-                         prepareMessage "C0MCQ870A" (translateId (coerce usr)
-                                                  <> " -> "
-                                                  <> T.pack (show p)))
-            >-> P.map encode
-            >-> P.toOutput output
+            >-> P.mapM_ (traverse_ (eventReply output state))
     sendClose c ("Goodbye" :: Text)
 
   where parse :: ByteString -> Either String Event
         parse = eitherDecode
 
-prepareMessage :: Text -> Text -> Value
-prepareMessage chan txt = object ["id" .= ("42" :: Text)
-                                 ,"type" .= ("message" :: Text)
-                                 ,"channel" .= chan
-                                 ,"text" .= show txt]
+prepareMessage :: MonadState State m => Text -> Text -> m Value
+prepareMessage chan txt = do
+  s <- get
+  put (s { stateNextId = stateNextId s + 1})
+  let i = stateNextId s
+  return $ object ["id" .= T.pack (show i)
+                  ,"type" .= ("message" :: Text)
+                  ,"channel" .= chan
+                  ,"text" .= show txt]
+
+eventReply :: (MonadIO m, MonadState State m) => P.Output ByteString -> State -> Event -> m ()
+eventReply output s (PresenceChangeEvt usr p) = do
+  msg <- prepareMessage (translateId s "bot-test") $ translateId s (coerce usr) <> " -> " <> T.pack (show p)
+  void (liftIO (P.atomically (P.send output (encode msg))))
+eventReply output s (MessageEvt mchan usr txt _) = do
+  let recipient = fromMaybe (coerce usr) mchan
+  msg <- prepareMessage recipient $ "Hi " <> translateId s (coerce usr) <> " I can hear you."
+  void (liftIO (P.atomically (P.send output (encode msg))))
+eventReply _ _ _ = return ()
+
+translateId :: State -> Text -> Text
+translateId state i = Map.findWithDefault i i (stateIdToName state)
